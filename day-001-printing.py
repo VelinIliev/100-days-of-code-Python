4c5abbdf8b292f51fdd8213f9cf3c74bd3b3fcb3
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2021-12-13 10:18:41 +0200

    day1

---
+++ day-001-printing.py
@@ -0,0 +1,9 @@
+print()
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("('what to print')")
+print()
+print("Day 1 - Python Print Function\nThe function is declared like this:\n('what to print')\n")
+
+name = input("What is your name?")
+print(f'Hello, {name.capitalize()}')
---
+++ day-001-variables.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+length = len(name)
+
+print(f'{name.capitalize()} is {length} characters long.')
