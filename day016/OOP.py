6a40d5471e7e06faa06ea51d050425a7e41cd82f
VelinIliev <veliniliev@abv.bg>
2021-12-23 10:40:24 +0200

    day16

---
+++ day016/OOP.py
@@ -0,0 +1,20 @@
+# # import turtle
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape('turtle')
+# timmy.color('coral')
+# timmy.forward(100)
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ['Pikachu', "Suirtle", "Charmander"])
+table.add_column("Type", ['Electric', "Water", "Fire"])
+table.align = "l"
+print(table)
